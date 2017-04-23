c0d723ee73533794643c04760b272250f82f14fd
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2017-04-23 10:07:48 -0400

    Defining a collections name for its schema

--- api/models/todosModel.js
+++ api/models/todosModel.js
@@ -3,20 +3,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodosSchema = new Schema({
-    title: {
-        type: String,
-        Required: 'Title is a mandatory field'
-    },
-    description: {
-        type: String
-    },
-    userName: {
-        type: String
-    },
-    timeStamp: {
-        type: Date,
-        default: Date.now
-    }
-});
+	title: {
+		type: String,
+		Required: 'Title is a mandatory field'
+	},
+	description: {
+		type: String
+	},
+	userName: {
+		type: String
+	},
+	timeStamp: {
+		type: Date,
+		default: Date.now
+	}
+}, { collection: 'todos' });
 
 module.exports = mongoose.model('Todos', TodosSchema);
