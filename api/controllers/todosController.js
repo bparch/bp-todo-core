b0ac0757293213872b53718c37f8dad0ca9ef43b
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2017-04-26 12:52:00 -0400

    Adding Error Handling

--- api/controllers/todosController.js
+++ api/controllers/todosController.js
@@ -3,50 +3,50 @@
 var mongoose = require('mongoose');
 var Todo = mongoose.model('Todos');
 
-exports.getAllTodos = function (request, response) {
-    Todo.find({}, function (error, todos) {
-        if (error) {
-            response.send(error);
-        }
-        response.json(todos);
-    });
+exports.getAllTodos = function (request, response, next) {
+	Todo.find({}, function (error, todos) {
+		if (error) {
+			return next(error);
+		}
+		response.json(todos);
+	});
 };
 
-exports.addTodo = function (request, response) {
-    var new_todo = new Todo(JSON.parse(request.body));
-    new_todo.save(function (error, todo) {
-        if (error) {
-            response.send(error);
-        }
-        response.json(todo);
-    });
+exports.addTodo = function (request, response, next) {
+	var new_todo = new Todo(JSON.parse(request.body));
+	new_todo.save(function (error, todo) {
+		if (error) {
+			return next(error);
+		}
+		response.json(todo);
+	});
 };
 
-exports.getTodo = function (request, response) {
-    Todo.findById(request.params.todoId, function (error, todo) {
-        if (error) {
-            response.send(error);
-        }
-        response.json(todo);
-    });
+exports.getTodo = function (request, response, next) {
+	Todo.findById(request.params.todoId, function (error, todo) {
+		if (error) {
+			return next(error);
+		}
+		response.json(todo);
+	});
 };
 
-exports.modifyTodo = function (request, response) {
-    Todo.findOneAndUpdate({ _id: request.params.todoId }, JSON.parse(request.body), { new: true }, function (error, todo) {
-        if (error) {
-            response.send(error);
-        }
-        response.json(todo);
-    });
+exports.modifyTodo = function (request, response, next) {
+	Todo.findOneAndUpdate({ _id: request.params.todoId }, JSON.parse(request.body), { new: true }, function (error, todo) {
+		if (error) {
+			return next(error);
+		}
+		response.json(todo);
+	});
 };
 
-exports.deleteTodo = function (request, response) {
-    Todo.remove({
-        _id: request.params.todoId
-    }, function (error, todo) {
-        if (error) {
-            response.send(error);
-        }
-        response.json({ message: 'Todo ' + request.params.todoId + ' was removed successfully' });
-    });
+exports.deleteTodo = function (request, response, next) {
+	Todo.remove({
+		_id: request.params.todoId
+	}, function (error, todo) {
+		if (error) {
+			return next(error);
+		}
+		response.json({ message: 'Todo ' + request.params.todoId + ' was removed successfully' });
+	});
 };
--- server.js
+++ server.js
@@ -12,14 +12,20 @@ mongoose.connect('mongodb://localhost/todo');
 
 // bodyParser will fetch request body and push it to request.body as a buffer
 app.use(bodyParser.raw({ type: 'application/json' }));
+routes(app);
 
 // This is error handling mechanism
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+app.use(function (request, response) {
+	let validRoute = new RegExp('\/todos*');
+	if (request.originalUrl !== validRoute) {
+		response.status(404).send({ url: request.originalUrl + ' is invalid' })
+	}
+});
 
-routes(app);
+app.use(function (error, request, response, next) {
+	console.error(error.stack)
+	response.status(500).send(error);
+});
 
 app.listen(8001);
 console.log('Server is listening on port 8001...');
