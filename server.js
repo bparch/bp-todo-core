4bb448cc0f807f9f738337101954b784f756ac51
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2017-04-27 12:26:43 -0400

    Adding MongoDB Cloud server

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var routes = require('./api/routes/todosRoutes');
 // Using Promises instead of Callbacks
 mongoose.Promise = global.Promise;
 
-// Connect to the local MongoDB
-mongoose.connect('mongodb://localhost/todo');
+// Connect to MongoDB server
+//mongoose.connect('mongodb://localhost/todo');
+mongoose.connect('mongodb://bparch:bparch@cluster0-shard-00-00-6jawd.mongodb.net:27017,cluster0-shard-00-01-6jawd.mongodb.net:27017,cluster0-shard-00-02-6jawd.mongodb.net:27017/todo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 // bodyParser will fetch request body and push it to request.body as a buffer
 app.use(bodyParser.raw({ type: 'application/json' }));
