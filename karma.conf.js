35fd10c445ba86328748ee3da66ec1083fb8fea4
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2017-04-30 10:42:55 -0400

    update test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,17 @@
 module.exports = function (config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine'],
-        files: [
-            'test/**/*.js'
-        ],
-        exclude: [
-        ],
-        preprocessors: {
-        },
-        reporters: ['progress'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
-        concurrency: Infinity
-    })
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine'],
+		files: ['test/**/*.js'],
+		exclude: [],
+		preprocessors: {},
+		reporters: ['progress'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Chrome'],
+		singleRun: false,
+		concurrency: Infinity
+	})
 }
