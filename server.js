db24852793bc4e3b9619287fb69f6d31b2db13b3
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2017-08-24 22:08:55 -0400

    Set CORS headers

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ var routes = require('./api/routes/todosRoutes');
 mongoose.Promise = global.Promise;
 
 // Connect to MongoDB server
-//mongoose.connect('mongodb://localhost/todo');
+// mongoose.connect('mongodb://localhost/todo');
 mongoose.connect('mongodb://bparch:bparch@cluster0-shard-00-00-6jawd.mongodb.net:27017,cluster0-shard-00-01-6jawd.mongodb.net:27017,cluster0-shard-00-02-6jawd.mongodb.net:27017/todo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+
+// Set CORS headers
+app.use(function (request, response, next) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // response.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // bodyParser will fetch request body and push it to request.body as a buffer
 app.use(bodyParser.raw({ type: 'application/json' }));
@@ -18,16 +27,16 @@ routes(app);
 
 // Checking for invalid URLs
 app.use(function (request, response) {
-	let validRoute = new RegExp('\/todos*');
-	if (request.originalUrl !== validRoute) {
-		response.status(404).send({ url: request.originalUrl + ' is invalid' })
-	}
+    let validRoute = new RegExp('\/todos*');
+    if (request.originalUrl !== validRoute) {
+        response.status(404).send({ url: request.originalUrl + ' is invalid' })
+    }
 });
 
 // Using error handling middleware
 app.use(function (error, request, response, next) {
-	console.error(error.stack);
-	response.status(500).send(error);
+    console.error(error.stack);
+    response.status(500).send(error);
 });
 
 app.listen(8001);
